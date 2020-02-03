9f6adad78cb320772f08d5701af6c13560356415
RabieLakhdar <lakhdar.rabie.benfaraji@gmail.com>
2020-02-03 17:02:30 +0100

    landing test

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,204 @@
 import React from "react";
-const Login = () => {
-    return<p>home page</p>
-}
-export default Login
+import Header from "../../components/header";
+import {
+  Container,
+  Text,
+  FieldWrapper,
+  Label,
+  Row,
+  SmallInput,
+  Button,
+  Forms,
+  Error
+} from "./style";
+import { useToasts } from 'react-toast-notifications'
+import { Formik, Form } from "formik";
+
+const _renderContent = () => {
+
+    const { addToast } = useToasts()
+
+  return (
+    <Forms>
+      <Formik
+        initialValues={{
+          fullname: null,
+          function: null,
+          phone: null,
+          mail: null,
+          town: null
+        }}
+        validateOnChange={false}
+        validateOnBlur={false}
+        validate={values => {
+          const errors = {};
+
+          if (!values.fullname) {
+            errors.fullname = "Please fill fullname please";
+          }
+          if (!values.function) {
+            errors.function = "Please fill function please";
+          }
+          if (!values.phone) {
+            errors.phone = "Fill your phone please";
+          }
+          if (!values.mail) {
+            errors.mail = "Fill your mail please";
+          }
+          if (!values.town) {
+            errors.town = "Fill your city please";
+          }
+          return errors;
+        }}
+        onSubmit={(values) => {
+            addToast(`Thank you for your submit, will text you soon`, {
+                appearance: 'success',
+                autoDismiss: true,
+              })
+        }}
+      >
+        {({ errors, values, handleChange, handleReset }) => (
+          <Form>
+            <Row direction="row" top={24} left={24} bottom={24} center>
+              <FieldWrapper>
+                <Label>
+                  Full Name<span>*</span>
+                </Label>
+                <FieldWrapper direction="row" marginRight={16}>
+                  <SmallInput
+                    type="string"
+                    width={300}
+                    height={51}
+                    onChange={handleChange}
+                    name="fullname"
+                    values={values}
+                    placeholder="Fill your Fillname"
+                    marginRight={8}
+                  />
+                </FieldWrapper>
+                {errors.fullname && <Error>{errors.fullname}</Error>}
+              </FieldWrapper>
+            </Row>
+
+            <Row direction="row" top={24} left={24} bottom={24} center>
+              <FieldWrapper>
+                <Label>
+                  Function<span>*</span>
+                </Label>
+                <FieldWrapper direction="row" marginRight={16}>
+                  <SmallInput
+                    type="string"
+                    width={300}
+                    height={51}
+                    onChange={handleChange}
+                    name="function"
+                    values={values}
+                    placeholder="Fill your Function"
+                    marginRight={8}
+                  />
+                </FieldWrapper>
+                {errors.function && <Error>{errors.function}</Error>}
+              </FieldWrapper>
+            </Row>
+
+            <Row direction="row" top={24} left={24} bottom={24} center>
+              <FieldWrapper>
+                <Label>
+                  Phone<span>*</span>
+                </Label>
+                <FieldWrapper direction="row" marginRight={16}>
+                  <SmallInput
+                    type="number"
+                    width={300}
+                    height={51}
+                    onChange={handleChange}
+                    name="phone"
+                    values={values}
+                    placeholder="Fill your Phone number"
+                    marginRight={8}
+                  />
+                </FieldWrapper>
+                {errors.phone && <Error>{errors.phone}</Error>}
+              </FieldWrapper>
+            </Row>
+
+            <Row direction="row" top={24} left={24} bottom={24} center>
+              <FieldWrapper>
+                <Label>
+                  Email<span>*</span>
+                </Label>
+                <FieldWrapper direction="row" marginRight={16}>
+                  <SmallInput
+                    type="mail"
+                    width={300}
+                    height={51}
+                    onChange={handleChange}
+                    name="mail"
+                    values={values}
+                    placeholder="Fill your mail adress"
+                    marginRight={8}
+                  />
+                </FieldWrapper>
+                {errors.mail && <Error>{errors.mail}</Error>}
+              </FieldWrapper>
+            </Row>
+
+            <Row direction="row" top={24} left={24} bottom={24} center>
+              <FieldWrapper>
+                <Label>
+                  City<span>*</span>
+                </Label>
+                <FieldWrapper direction="row" marginRight={16}>
+                  <SmallInput
+                    type="string"
+                    width={300}
+                    height={51}
+                    onChange={handleChange}
+                    name="town"
+                    values={values}
+                    placeholder="Fill your city"
+                    marginRight={8}
+                  />
+                </FieldWrapper>
+                {errors.town && <Error>{errors.town}</Error>}
+              </FieldWrapper>
+            </Row>
+
+            <Row center>
+              <Button
+                width="146px"
+                height="51px"
+                border="#0054df"
+                type="submit"
+              >
+                Send
+              </Button>
+            </Row>
+
+          </Form>
+        )}
+      </Formik>
+    </Forms>
+  );
+};
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Container>
+        <Text color="#3e3f42" size="24px">
+          Inscrivez vous et tentez de gagner un ticket pour la journée Agile En
+          Med sur le thème "Agilité et Culture d'Entreprise" qui aura lieu le 8
+          février au campus universitaire de l'Ecole Centrale Casablanca.
+        </Text>
+        <Text color="#ff1515" size="30px" top="50px">
+          Feel Free to get ready
+        </Text>
+      </Container>
+      {_renderContent()}
+    </>
+  );
+};
+
+export default Home;
---
+++ src/pages/home/style.js
@@ -0,0 +1,123 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 0 auto;
+  padding: 5px;
+  border-radius: 5px;
+  width: 70%;
+  text-align: center;
+  margin-top:80px;
+  display: grid;
+`;
+
+export const Text = styled.span`
+  font-family: Rubik;
+  font-size: ${props => props.size};
+  font-weight: 600;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: ${props => props.color};
+  margin-top: ${props => props.top};
+`;
+
+export const Error = styled.span`
+  color: #f50057;
+  font-size: 16px;
+  font-family: serif;
+`
+export const Button = styled.button`
+  text-align: center;
+  color: ${props => (props.color ? props.color : '#fff')};
+  width: ${props => (props.width ? props.width : '155px')};
+  height: ${props => (props.height ? props.height : '40px')};
+  border: ${props => props.border};
+  border-radius: 6px;
+  pointer-events: auto;
+  cursor: pointer;
+  background-color: ${props =>
+    props.background ? props.background : '#0054df'};
+  font-family: Rubik;
+  font-size: 16px;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  &:focus {
+    outline: none;
+  }
+`
+export const Label = styled.label`
+  text-transform: uppercase;
+  margin-bottom: 16px;
+  display: block;
+  width: auto;
+  height: 17px;
+  font-family: Rubik;
+  font-size: 14px;
+  font-weight: normal;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: 0.16px;
+  color: #808794;
+  span {
+    color: #df0000;
+  }
+`
+
+export const FieldWrapper = styled.div`
+  display: flex;
+  flex-direction: ${({ direction }) => (direction ? direction : 'column')};
+  margin-right: ${({ marginRight }) => marginRight && marginRight}px;
+  width: ${({ width }) => (width ? `${width}px` : 'auto')};
+`
+export const Row = styled.span`
+  display: flex;
+  margin-top: ${props => props.top}px;
+  margin-bottom: ${props => props.bottom}px;
+  margin-left: ${props => props.left}px;
+  margin-right: ${props => props.right}px;
+  align-items: center;
+  justify-content: ${props => (props.center ? 'center' : 'flex-start')};
+  cursor: ${props => props.pointer && 'pointer'};
+  flex-direction: ${({ direction }) => (direction ? direction : 'initial')};
+`
+export const Input = styled.input`
+  border-radius: 4px;
+  border: solid 1px #e0e3e8;
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: 400;
+  font-style: normal;
+  font-stretch: normal;
+  letter-spacing: normal;
+  color: #3e3f42;
+  padding: 16px 0 16px 16px;
+  width: 82px;
+  padding: 5px 0px;
+  line-height: 10px;
+  text-align: center;
+  &:focus {
+    border: solid 2px #0054df;
+    outline: none;
+  }
+`
+
+export const SmallInput = styled(Input)`
+  width: ${({ width }) => width && width}px;
+  height: ${({ height }) => height && height}px;
+  margin-right: ${({ marginRight }) => marginRight && marginRight}px;
+  margin-bottom: 0;
+`
+
+export const Forms = styled.div`
+   border: 1px solid red;
+   width: 50%;
+   margin: 0 auto;
+   border-radius: 10px;
+   padding-bottom: 15px;
+   margin-bottom:45px;
+   margin-top: 20px;
+`
