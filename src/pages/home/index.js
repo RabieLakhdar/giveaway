4243d474209b8e8725ee09034663213aa9c22395
zakaria <zbenaddi@maltem.com>
2020-02-03 16:20:24 +0100

    initial

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,72 @@
-import React from "react";
-const Login = () => {
-    return<p>home page</p>
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import axios from 'axios';
+class Register extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            first_name: '',
+            phone: '',
+            email: '',
+            Fonction: '',
+            city:''
+        }
+    }
+    render() {
+        return (
+            <div>
+                <MuiThemeProvider>
+                    <div>
+                        <AppBar
+                            title="Maltem Academy - Giveaway
+                            "
+                            
+                        />
+                        <TextField
+                            hintText="Votre réponse"
+                            floatingLabelText="Nom et Prénom"
+                            onChange={(event, newValue) => this.setState({ first_name: newValue })}
+                        />
+                        <br />
+                        <TextField
+                            hintText="Votre réponse"
+                            type="phone"
+                            floatingLabelText="télephone"
+                            onChange={(event, newValue) => this.setState({ phone: newValue })}
+                        />
+                     
+                        <br />
+                        <TextField
+                            hintText="Votre réponse"
+                            type="email"
+                            floatingLabelText="Adresse e-mail"
+                            onChange={(event, newValue) => this.setState({ email: newValue })}
+                        />
+                        <br />
+                        <TextField
+                            type="Fonction"
+                            hintText="Votre réponse"
+                            floatingLabelText="Fonction"
+                            onChange={(event, newValue) => this.setState({ Fonction: newValue })}
+                        />
+                        <br />
+                        <TextField
+                            hintText="Votre réponse"
+                            floatingLabelText="ville"
+                            onChange={(event, newValue) => this.setState({ city: newValue })}
+                        />
+                                                <br />
+                        <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)} />
+                    </div>
+                </MuiThemeProvider>
+            </div>
+        );
+    }
 }
-export default Login
+const style = {
+    margin: 15,
+};
+export default Register;
